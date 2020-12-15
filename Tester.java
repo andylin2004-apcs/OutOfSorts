fb9fe347ad22c0430c3c72c86304d51d538288ff
Andy Lin <alin21@stuy.edu>
2020-12-15 14:24:21 -0500

    made it literally compare the arrays to see if they're the same

--- Tester.java
+++ Tester.java
@@ -4,15 +4,21 @@ public class Tester{
   public static void main(String[] args) {
     Random rng = new Random();
 
-    int[] testCase = new int[rng.nextInt() % 1000];
-
+    int[] testCase = new int[Math.abs(rng.nextInt() % 1000)];
+    int[] testCaseResult = new int[testCase.length];
     // rng = new Random(100);
     for (int i = 0; i<testCase.length; i++){
       testCase[i] = rng.nextInt() % 1000;
+      testCaseResult[i] = testCase[i];
     }
 
     System.out.println(Arrays.toString(testCase));
 
-    Sorts.bubbleSort(testCase);
+    Sorts.bubbleSort(testCaseResult);
+
+    Arrays.sort(testCase);
+    System.out.println(Arrays.toString(testCase));
+
+    System.out.println(Arrays.toString(testCase).equals(Arrays.toString(testCaseResult)));
   }
 }
